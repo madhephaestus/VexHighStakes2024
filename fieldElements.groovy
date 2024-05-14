48add35d6ccd3bb056786fd4be97809d754778bc
Kevin Harrington <kharrington@bancroftschool.org>
2024-05-14 12:29:22 -0400

    adding mass

--- fieldElements.groovy
+++ fieldElements.groovy
@@ -10,6 +10,7 @@ double inner = 76.2/2.0
 double outer = 177.8/2.0
 double diff  = outer - inner
 println diff
+double mass =0.005
 outer = inner+diff/2
 CSG t = new  Toroid(inner, outer , 40, 40).toCSG()
 			.setColor(javafx.scene.paint.Color.BLUE)
@@ -22,6 +23,6 @@ cubeSlice=cubeSlice.intersect(cubeSlice.rotz(sliceAngle))
 for(double i=0;i<360;i+=sliceAngle){
 	parts.add(t.intersect(cubeSlice.rotz(i)).movex(300))
 }
-parts.forEach{it.setName("vexRing")};
+parts.forEach{it.setName("vexRing").getStorage().set("massKg", mass/parts.size());};
 return parts
 
