7ea13ac956c145f4fe8bb4b9aef4714bc5dccbb6
Kevin Harrington <kharrington@bancroftschool.org>
2024-05-14 11:06:48 -0400

    adding ring parts

--- fieldElements.groovy
+++ fieldElements.groovy
@@ -1,5 +1,6 @@
 
 import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
 import eu.mihosoft.vrl.v3d.Toroid
 
 // code here
@@ -10,10 +11,17 @@ double outer = 177.8/2.0
 double diff  = outer - inner
 println diff
 outer = inner+diff/2
-
 CSG t = new  Toroid(inner, outer , 40, 40).toCSG()
 			.setColor(javafx.scene.paint.Color.BLUE)
 			.rotx(90)
-			
-return t
+double sliceAngle = 360/10
+CSG cubeSlice = new Cube(outer*2,outer*2,diff).toCSG()
+				.toXMin()
+				.toYMin()
+cubeSlice=cubeSlice.intersect(cubeSlice.rotz(sliceAngle))
+for(double i=0;i<360;i+=sliceAngle){
+	parts.add(t.intersect(cubeSlice.rotz(i)).movex(300))
+}
+parts.forEach{it.setName("vexRing")};
+return parts
 
