8724f52e9855350b5254db68eed9d8a7792677cd
Kevin Harrington <kharrington@bancroftschool.org>
2024-05-21 10:32:24 -0400

    bottom of the weeble

--- doughnut.groovy
+++ doughnut.groovy
@@ -1,1 +1,26 @@
-// code here
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
+import eu.mihosoft.vrl.v3d.Toroid
+
+ArrayList<CSG> parts = []
+double inner = 76.2/2.0
+double outer = 177.8/2.0
+double diff  = outer - inner
+//println diff
+double mass =0.122
+
+CSG t = new  Toroid(inner, outer , 40, 40).toCSG()
+			.setColor(javafx.scene.paint.Color.BLUE)
+			.rotx(90)
+double sliceAngle = 360/15
+CSG cubeSlice = new Cube(outer*2,outer*2,diff).toCSG()
+				.toXMin()
+				.toYMin()
+cubeSlice=cubeSlice.intersect(cubeSlice.rotz(90-sliceAngle))
+for(double i=0;i<360;i+=sliceAngle){
+	parts.add(t.intersect(cubeSlice.rotz(i)))
+	//parts.add(cubeSlice.rotz(i))
+}
+parts.forEach{it.setName("vexRing").getStorage().set("massKg", mass/parts.size());};
+return parts
+
--- mobileSpike.groovy
+++ mobileSpike.groovy
@@ -1,6 +1,15 @@
-// code here
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cylinder
+import eu.mihosoft.vrl.v3d.Sphere
 
 double bottomWidth = 300
-double bottomHeight = 75
-double bottomRad =1000
+double bottomHeight = 50
+double bottomRad =250
 
+CSG weeble = new Sphere(bottomRad,60,80).toCSG().toZMin()
+CSG weebleCutter = new Cylinder(bottomWidth/2, bottomHeight).toCSG()
+weeble=weeble.intersect(weebleCutter)
+		.setColor(javafx.scene.paint.Color.color(0.2,0.2,0.2))
+
+
+return weeble
