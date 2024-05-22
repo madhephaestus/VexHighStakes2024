9a9bdcb7dfd61409c21dc1715e16986c8bd667e8
Kevin Harrington <kharrington@bancroftschool.org>
2024-05-22 13:33:13 -0400

    spike created

--- mobileSpike.groovy
+++ mobileSpike.groovy
@@ -1,15 +1,29 @@
 import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cylinder
+import eu.mihosoft.vrl.v3d.Hexagon
 import eu.mihosoft.vrl.v3d.Sphere
 
-double bottomWidth = 300
-double bottomHeight = 50
-double bottomRad =250
+double bottomWidth = 254
+double bottomHeight = 30.1
+double bottomRad =410
+double lipHeight = 44.45-bottomHeight
+double lipInnerWidth =190.5
+double totalHeight = 368.3-bottomHeight
+double massKG = 0.948
 
-CSG weeble = new Sphere(bottomRad,60,80).toCSG().toZMin()
-CSG weebleCutter = new Cylinder(bottomWidth/2, bottomHeight).toCSG()
+CSG weeble = new Sphere(bottomRad,100,100).toCSG().toZMin()
+CSG weebleCutter = new Hexagon(bottomWidth, bottomHeight).toCSG()
 weeble=weeble.intersect(weebleCutter)
-		.setColor(javafx.scene.paint.Color.color(0.2,0.2,0.2))
+		.setColor(javafx.scene.paint.Color.GOLD)//color(0.2,0.2,0.2))
+CSG lipOuter = new Hexagon(bottomWidth,lipHeight).toCSG()
+CSG lipInner = new Hexagon(lipInnerWidth,lipHeight).toCSG()
+CSG lip = lipOuter.difference(lipInner)
+				.movez(bottomHeight)
+				.setColor(javafx.scene.paint.Color.web("#CFFF04"))
+CSG Pole = new Cylinder(12.5, totalHeight).toCSG().movez(bottomHeight)
+				.setColor(javafx.scene.paint.Color.web("#CFFF04"))
+weeble.setMassKG(massKG-0.02)
+Pole.setMassKG(0.01)
+lip.setMassKG(0.01)
 
-
-return weeble
+return [weeble,lip,Pole].collect{it.setName("MobileSpike")}
